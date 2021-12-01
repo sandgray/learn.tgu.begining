0c3f55cff7c400b489cb87556ba140d26b136160
sandgray <sergey.andriyuk@yandex.ru>
2021-12-01 20:53:38 +1000

    cpc 5

--- app/src/main/java/learh/gtu/begin/lesson1/MainActivity.kt
+++ app/src/main/java/learh/gtu/begin/lesson1/MainActivity.kt
@@ -1,11 +1,73 @@
 package learh.gtu.begin.lesson1
 
+import android.content.res.Resources
+import android.hardware.biometrics.BiometricManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
+lateinit var stringsPoem:Array<String>
+var activityChangeState:Int = 0
+
+fun writeLog(msg: String)
+{
+    Log.d(TAG, msg)
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val res = getResources()
+        stringsPoem = res.getStringArray(R.array.arrayStringPoem)
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        writeLog(stringsPoem[activityChangeState].toString())
+        activityChangeState += 1
+    }
+
+
 }
